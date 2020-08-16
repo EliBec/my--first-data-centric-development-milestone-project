6a0fee9cd0b665c82195f397205ee2b68fa5310a
Eliana Beckinsale <Elianageral@hotmail.com>
2020-08-16 15:43:31 +0000

    Added env.py and gitnore. Also, set the IP, Port and app instances in app.py

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+import os
+from os import path
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
+# instatiate Flask application
 app = Flask(__name__)
+
+# import env.py 
+if path.exists('env.py'):
+    import env
+
+# retrieve environment variables
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+# instatiate Mongo application
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+
 if __name__ == "__main__":
     app.run()
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
